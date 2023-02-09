bf9cdde275116ad3263e70c3291f048d70f40612
audreysoares <amsoares@hawaii.edu>
2023-02-09 01:53:56 -1000

    Finished in 36:49

---
+++ sumofnumbers2.js
@@ -0,0 +1,39 @@
+const testnums = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testnums));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testnums));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(testnums));
+
+function sumTheSimpleWay(nums) {
+  const total = _.reduce(nums, function (memo, num) { return memo + num; }, 0);
+  return total;
+}
+
+console.log(sumTheSimpleWay(testnums));
